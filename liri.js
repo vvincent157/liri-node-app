0a49efb00ece0c48b206326b0cb9658739fe1ae8
Victoria <Victoria@Victorias-MacBook-Pro.local>
2018-12-10 00:46:43 -0600

    updated spotify

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-var Spotify = require('node-spotify-api');
+var spotify = require('node-spotify-api');
 var axios = require("axios");
 var moment = require("moment");
 
@@ -27,6 +27,14 @@ axios.get(queryUrl).then(
   );
          break;
     case "spotify-this-song":
+    spotify
+  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+  .then(function(data) {
+    console.log(data); 
+  })
+  .catch(function(err) {
+    console.error('Error occurred: ' + err); 
+  });
         console.log("spotify")
         break;
     case "movie-this":
